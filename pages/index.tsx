7e65d0ebc445ce21da2e2db76af2578100ebc0b2
st43 <45909001+0si43@users.noreply.github.com>
2021-06-16 11:58:36 +0900

    記事ページのガワを作成した

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,21 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Header titlePre="Home" />
+      <div className={styles.container}>
+        <div className={styles.profileImage}>
+          <Image src="/profile.png" alt="Avatar" width={200} height={200} />
+        </div>
 
-      <div className={styles.profileImage}>
-        <Image src="/profile.png" alt="Avatar" width={200} height={200} />
+        <main className={styles.main}>
+          <h1 className={styles.title}>蔀のページへようこそ</h1>
+        </main>
+
+        <footer className={styles.footer}>
+          Copyright © Shetommy All Rights Reserved.
+        </footer>
       </div>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>蔀のページへようこそ</h1>
-      </main>
-
-      <footer className={styles.footer}>
-        Copyright © Shetommy All Rights Reserved.
-      </footer>
-    </div>
+    </>
   )
 }
