ec37efa688df027d92632fe856b42ce88db52881
st43 <45909001+0si43@users.noreply.github.com>
2021-06-17 16:32:19 +0900

    アクティビティを加筆した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,35 +25,42 @@ export default function Home() {
               <div className={styles.name}>蔀</div>
               （しとみ）
               <ul>
-                <li>iOS開発がメインのソフトウェアエンジニア</li>
+                <li>iOSエンジニア</li>
                 <li>楽天イーグルスファン</li>
                 <br></br>
-                <li>Web上の成果物が散らばってきたので、ここに集約します</li>
                 <li>
-                  一年に一個大きなソフトウェア開発ができるようにがんばるのが目標です
+                  個人開発の成果物とWeb上に公開した文章のなかでよかったものを残していきます
                 </li>
+                <li>年に一個大きな成果が出せるようにがんばっています</li>
               </ul>
             </div>
           </div>
-          <br></br>
-          <br></br>
-          <br></br>
-          Career<br></br>
-          2019 - iOSエンジニア<br></br>
-          <br></br>
-          Works<br></br>
-          2020 Pieces of Paper<br></br>
-          <br></br>
-          Skill Set<br></br>
-          iOS開発<br></br>
-          <br></br>
-          Activity<br></br>
-          2020 - note<br></br>
-          2009 - 2019 個人ブログ<br></br>
-          <br></br>
-          Development<br></br>
-          色々個人的に試す環境です<br></br>
-          <br></br>
+          <div className={styles.contents}>
+            <h1>Works</h1>
+            <h2>2020</h2>
+            <Image
+              src="/pieces_of_paper.png"
+              alt="Pieces of Paper's app icon"
+              width={48}
+              height={48}
+            />
+            <p>Pieces of Paper</p>
+            <h1>Activity</h1>
+            <p>2020 - note</p>
+            <p>2009 - 2019 個人ブログ</p>
+
+            <h1>Skill Set</h1>
+            <p>iOS開発</p>
+            <p>Unity</p>
+            <h1>Career</h1>
+            <h2>2019 - </h2>
+            <p>iOSエンジニア</p>
+            <h2>2014 - 2019</h2>
+            <p> 金融系システムエンジニア</p>
+
+            <h1>Development</h1>
+            <p>色々個人的に試す環境です</p>
+          </div>
         </main>
 
         <footer className={styles.footer}>
