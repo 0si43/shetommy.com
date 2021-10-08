4069d70f31caa089f38ccbe4aebf691294c0017d
0si43 <symn43@gmail.com>
2021-10-08 15:41:25 +0900

    OGPのメタ情報を設定した #8

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,9 +16,30 @@ const Header = ({ titlePre = '' }) => {
       <Head>
         <link rel="icon" href="/profile.png" />
         <title>{titlePre} </title>
-        <meta name="description" content="Shetommy's portfolio website" />
-        <meta name="og:title" content="蔀のポートフォリオサイト" />
-        {/* <meta property="og:image" content={ogImageUrl} /> */}
+        <meta
+          name="description"
+          content="Shetommy's portfolio website and personal blog"
+        />
+        <meta property="og:title" content={titlePre} />
+        <meta
+          property="og:type"
+          content={titlePre == 'Home' ? 'website' : 'article'}
+        />
+        <meta
+          property="og:description"
+          content={
+            titlePre == 'Home'
+              ? '蔀のポートフォリオサイト'
+              : titlePre == 'Articles'
+              ? '蔀の個人ブログ'
+              : 'Written by 蔀'
+          }
+        />
+        <meta
+          property="og:image"
+          content="https://www.shetommy.com/_next/image?url=%2Fprofile.png&w=640&q=75"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
