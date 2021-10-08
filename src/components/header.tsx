398014965f21996109aae059958dea1acda3efb2
0si43 <symn43@gmail.com>
2021-10-08 13:55:43 +0900

    twemp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,19 @@ const Header = ({ titlePre = '' }) => {
         <title>{titlePre} </title>
         <meta name="description" content="Shetommy's portfolio website" />
         <meta name="og:title" content="蔀のポートフォリオサイト" />
-        {/* <meta property="og:image" content={ogImageUrl} /> */}
+        <meta property="og:title" content={titlePre} />
+        <meta
+          property="og:description"
+          content="Shetommy's portfolio website"
+        />
+        <meta
+          property="og:image"
+          content="https://www.shetommy.com/_next/image?url=%2Fprofile.png&w=640&q=75"
+        />
+        <meta
+          name="twitter:card"
+          content="https://www.shetommy.com/_next/image?url=%2Fprofile.png&w=640&q=75"
+        />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
