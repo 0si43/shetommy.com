58c3962c07bb18d23faf1aaa35e40bf9fdece84a
st43 <45909001+0si43@users.noreply.github.com>
2021-10-18 22:10:46 +0900

    更にAnyワーニングが出たので対処した

--- src/components/notion.tsx
+++ src/components/notion.tsx
@@ -24,11 +24,13 @@ export const getBlocks = async (blockId: string) => {
   let cursor: undefined | string = undefined
 
   while (true) {
-    const { results, next_cursor } = await notion.blocks.children.list({
+    const blocksList = await notion.blocks.children.list({
       start_cursor: cursor,
       block_id: blockId,
     })
-    blocks.push(...results)
+    blocks.push(...blocksList.results)
+
+    const next_cursor = blocksList.next_cursor as string | null
     if (!next_cursor) {
       break
     }
