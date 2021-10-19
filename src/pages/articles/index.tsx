a0913698020a4815198757ebee8ee68d40fcde6d
nakajima <symn43@gmail.com>
2021-10-19 12:42:15 +0900

    article/indexまわりの型エラーに対応した

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,12 +1,8 @@
 import Header from '../../components/header'
 import Link from 'next/link'
-import { getDatabase } from '../../components/notion'
+import { getDatabase, getPageTitle, getPageDate } from '../../components/notion'
+import type { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints.d'
 import { TextComponent } from './[title]'
-// import type {
-//   DatePropertyValue,
-//   TitlePropertyValue,
-//   RichTextText,
-// } from '@notionhq/client/build/src/api-types'
 import styles from '../../styles/articles/index.module.css'
 import Footer from '../../components/footer'
 import { InferGetStaticPropsType } from 'next'
@@ -22,40 +18,30 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      posts: database,
+      db: database,
     },
     revalidate: 1,
   }
 }
 
-export default function Home(props: Props) {
+export default function Home({ db }: Props) {
   return (
     <div>
       <main className={styles.container}>
         <Header titlePre="Articles" />
         <h2 className={styles.heading}>All Posts</h2>
         <ol className={styles.posts}>
-          {props.posts.map((post) => {
-            const titleProperty = post.properties.Name
-            let title = ''
-            if (titleProperty.type == 'title') {
-              const titleRichText = titleProperty.title
-              title = titleRichText[0].plain_text
-            }
-
-            let dateString = post.last_edited_time
-            const publishDateObject = post.properties['publish date']
-            if (publishDateObject.type == 'date') {
-              dateString = publishDateObject.date.start
-            }
-            const date = new Date(dateString).toLocaleDateString()
+          {db.map((post) => {
+            const title = getPageTitle(post.properties)
+            const date = getPageDate(post)
 
             return (
               <li key={title} className={styles.post}>
                 <h3 className={styles.postTitle}>
                   <Link href={`/articles/${title}`}>
                     <a>
-                      <TextComponent richTexts={titleRichText} />
+                      {/* <TextComponent richTexts={post.properties.Name} /> */}
+                      {title}
                     </a>
                   </Link>
                 </h3>
