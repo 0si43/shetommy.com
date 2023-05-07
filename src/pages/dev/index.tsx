9f7659c6d4e88b5653e7da392f42c59ea75dcd00
0si43 <symn43@gmail.com>
2023-05-07 17:50:50 +0900

    added privacy policy

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -1,5 +1,21 @@
-function Dev() {
-  return <p>Dev</p>
-}
+import Link from 'next/link'
 
-export default Dev
+export default function Dev() {
+  return (
+    <>
+      <h1>
+        プライバシーポリシー
+      </h1>
+      <p>
+        <Link href="/privacy_policy/wristcounter.html">
+        <a>WristCounter</a>
+        </Link>
+      </p>
+      <p>
+        <Link href="/privacy_policy/wristcounter_jp.html">
+        <a>WristCounter（日本語）</a>
+        </Link>
+      </p>
+    </>
+  )
+}
