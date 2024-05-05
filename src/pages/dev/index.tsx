1e731f1af347f63ca4d4523fe8e843ef7e783d9e
0si43 <symn43@gmail.com>
2024-05-05 21:15:13 +0900

    fix ogp block bug

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -2,20 +2,20 @@ import Link from 'next/link'
 
 export default function Dev() {
   return (
-    <>
-      <h1>
-        プライバシーポリシー
-      </h1>
-      <p>
-        <Link href="/privacy_policy/wristcounter.html">
-        WristCounter
-        </Link>
-      </p>
-      <p>
-        <Link href="/privacy_policy/wristcounter_jp.html">
-        WristCounter（日本語）
-        </Link>
-      </p>
+      <>
+        <h1>
+          プライバシーポリシー
+        </h1>
+        <p>
+          <Link href="/privacy_policy/wristcounter.html">
+          WristCounter
+          </Link>
+        </p>
+        <p>
+          <Link href="/privacy_policy/wristcounter_jp.html">
+          WristCounter（日本語）
+          </Link>
+        </p>
     </>
   )
 }
