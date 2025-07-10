23414e402387a6178db9aec6bfe96a326f5c3428
0si43 <symn43@gmail.com>
2025-07-10 13:00:42 +0900

    fix profile

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,28 +1,36 @@
 import BuyMeACoffeeWidget from '../components/coffee'
 import Image from 'next/image'
-import Link from 'next/link'
 import OtherSites from '../components/otherSites'
 import styles from '../styles/home/profile.module.css'
 
 export default function Profile() {
   return (
     <div className={styles.profile}>
-      <div className={styles.left}>
-        <div className={styles.image}>
-          <Image src="/profile.png" alt="Avatar" width={200} height={200} />
-        </div>
-        <OtherSites />
-        <BuyMeACoffeeWidget />
-      </div>
       <div className={styles.right}>
-        <div className={styles.name}>蔀（しとみ）</div>
-        <div className={styles.name}>Shetommy</div>
+        <div className={styles.image}>
+          <Image src="/profile.png" alt="Tsubakuro icon" width={140} height={140} />
+        </div>
+        <p>
+          本サイトshetommy.comは蔀（しとみ）の個人サイトです。<br />
+          ソフトウェアエンジニアとしてのポートフォリオサイトであり、個人ブログでもあります。<br />
+          様々なプラットフォーム散らばった成果物をこのサイトに集約します。<br />
+        </p>
+        <h1>各種アカウント一覧</h1>
+        <p>
+          下記が他サイトで動かしているアカウントです。
+        </p>
+        <OtherSites />
         <ul>
-          <li>iOSエンジニア</li>
-          <li>楽天イーグルスファン</li>
-          <br></br>
-          <li>ポートフォリオサイト + 個人ブログ</li>
-          <li>個人開発の成果物とよかった文章を残していきます</li>
+          <li>SNSはTwitterがメインです。もし連絡取りたい方はリプライいただければDM開放します。</li>
+          <li>GitHubアカウントで個人開発のソースコードを公開しています。</li>
+          <li>
+            Zennで技術記事の公開を行っています。<br />
+            （昔はQiitaに書いていましたが、移行しました。過去記事はQiitaに残っています）
+          </li>
+          <li>
+            noteはなるべく誰にでも読める記事（not技術記事）を書いています。<br />
+            noteにはたくさん読まれたい内容を書き、そうでないときは個人ブログに書くという運用にしております。
+          </li>
         </ul>
       </div>
     </div>
