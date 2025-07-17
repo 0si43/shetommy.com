8bee5bd80cffc229da6166ad6590018be11a196e
0si43 <symn43@gmail.com>
2025-07-17 15:29:55 +0900

    remove git file ref

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/footer.module.css'
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      Copyright © Shetommy All Rights Reserved.
-    </footer>
-  )
-}
